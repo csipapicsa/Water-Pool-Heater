b06b4d5aab2e20d21e91cb27279295e5df69148a
Gergo Gyori <gergo.gyori@gmail.com>
2024-07-30 10:24:55 +0200

    Validator + temp sensor fix

---
+++ arduino_code/ReadingsValidation/ReadingsValidation.ino
@@ -0,0 +1,24 @@
+# include "PhotoSensor.h"
+# include "TempSensorHeaders.h"
+# include "WaterPump.h"
+# include "WaterLevelSensorHeaders.h"
+//#include "Uart_eeprom_settings.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("-------------------");
+  Serial.println("Get Photo Sensor");
+  Serial.println(getSolarvoltage());
+  Serial.println("Get Water Level Sensor");
+  Serial.println(getWaterLevel());
+  Serial.println("Get Temperature Sensor");
+  Serial.println(getWaterTemperature());
+  delay(1000);
+
+}
