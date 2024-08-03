04b5f8265c01c78590389079161d8e1973e6d032
Gergo Gyori <gergo.gyori@gmail.com>
2024-08-03 14:44:41 +0200

    TempSensorHeaders updated

--- arduino_code/MotorControl/MotorPumpControl/MotorPumpControl.ino
+++ arduino_code/MotorControl/MotorPumpControl/MotorPumpControl.ino
@@ -1,20 +1,28 @@
-# include "MainSwitch.h"
+#include "MainSwitch.h"
+#include "TempSensorHeaders.h"
+
+double waterTempTarget = 30;
 
 void setup() {
   initMainSwitch();
+  initTempSensor();
   pinMode(A3, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-    Serial.println(MainSwitchState());
-    if (MainSwitchState())
+  Serial.println(MainSwitchState());
+  if (MainSwitchState()) {
+    if (getWaterTemperature() < waterTempTarget)
     {
-      digitalWrite(A3, HIGH); // Turn the pin on           // Wait for 1 second
+      digitalWrite(A3, HIGH); // Turn the pin on
     }
-    else {
-      digitalWrite(A3, LOW);  // Turn the pin off         // Wait for 1 second
-
+    else if (getWaterTemperature() > waterTempTarget) {
+      digitalWrite(A3, LOW);
     }
-
+    
+  } else {
+    digitalWrite(A3, LOW);  // Turn the pin off
+  }
+  delay(500);
 }
